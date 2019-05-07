5551078df2e580774f61511a7c5c59beadd9f3c5
Daniel <kontakt@danielrajic.de>
2019-05-07 22:02:34 +0200

    link repo method

--- src/main/java/de/rajic/springboot/training/repository/LinkRepository.java
+++ src/main/java/de/rajic/springboot/training/repository/LinkRepository.java
@@ -4,8 +4,14 @@ import de.rajic.springboot.training.domain.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LinkRepository extends JpaRepository<Link, Long> {
+
+    Link findByTitle(String title);
+
+    List<Link> findAllByTitleLikeOrOrderByCreationDateDesc(String title);
 
 
 }
